8e63aed2366932f88626bb7830af6c10129fb005
behappiness <44143535+behappiness@users.noreply.github.com>
2022-12-01 06:00:50 +0100

    First working ContentResolver

--- src/android/OpenscaleIntegration.java
+++ src/android/OpenscaleIntegration.java
@@ -6,11 +6,20 @@ import org.apache.cordova.CallbackContext;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import android.database.Cursor;
+import android.net.Uri;
+import android.Manifest;
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
 
-/**
- * This class echoes a string called from JavaScript.
- */
 public class OpenscaleIntegration extends CordovaPlugin {
+    final String usersContentUri = "content://com.health.openscale.provider/users";
+    final String measurementsContentUri = "content://com.health.openscale.provider/measurements/$ID";
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
@@ -23,10 +32,27 @@ public class OpenscaleIntegration extends CordovaPlugin {
     }
 
     private void coolMethod(String message, CallbackContext callbackContext) {
+
+        ContentResolver contentResolver = cordova.getActivity().getContentResolver();
+        if(!cordova.hasPermission("com.health.openscale.READ_WRITE_DATA"))
+            cordova.requestPermission(this, 1, "com.health.openscale.READ_WRITE_DATA");
+        final String[] projection = new String[]{"id", "username"};
+        Uri uri = Uri.parse(usersContentUri);
+        Cursor query = contentResolver.query(uri, null, null, null, null);
+        String ret = "";
+        for (String s : query.getColumnNames()) {
+            ret = ret+" "+s;
+        }
+        callbackContext.success(ret);
+
+
+        /*
         if (message != null && message.length() > 0) {
             callbackContext.success(message);
         } else {
             callbackContext.error("Expected one non-empty string argument.");
-        }
+        }*/
     }
+
+
 }
